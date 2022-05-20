2ccf24f7433d9fd14e83b2507be3b9688caa0b55
KIPSOS ONESMUS <kipsosonesmus@KIPSOSs-MacBook-Pro.local>
2022-05-20 12:06:59 +0300

    modified the post model

--- posts/models.py
+++ posts/models.py
@@ -6,16 +6,15 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
-    title = models.CharField(max_length=250)
-    company_name = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
-    products_and_services = models.CharField(max_length=50)
-    contact = models.CharField(max_length=50)
-    application_requirements = models.CharField(max_length=50)
-    application_process = models.CharField(max_length=50)
-    application_deadline = models.CharField(max_length=50)
-    contact = models.DateTimeField(auto_now_add=True)
+    company_name = models.CharField(max_length=50, null=True, blank=True)
+    location = models.CharField(max_length=50, null=True, blank=True)
+    category = models.CharField(max_length=50, null=True, blank=True)
+    products_and_services = models.CharField(max_length=50, null=True, blank=True)
+    contact = models.CharField(max_length=50, null=True, blank=True)
+    application_requirements = models.CharField(max_length=50, null=True, blank=True)
+    application_process = models.CharField(max_length=50, null=True, blank=True)
+    application_deadline = models.CharField(max_length=50, null=True, blank=True)
+    contact = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
