59474ac51612f20d18f0c5d380e8bdd02d8de236
KIPSOS ONESMUS <kipsosonesmus@KIPSOSs-MacBook-Pro.local>
2022-05-30 17:51:06 +0300

    debug = true

--- slickskills/settings.py
+++ slickskills/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6b&x*zjjnebxunuyd%_um74+jxcywn)jr21dswm&kmb()$w5(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://slick-skills.herokuapp.com', '127.0.0.1']
 
