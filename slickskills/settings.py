cc1caa72b1e1a4b063382f2c3a3cb19bc6d31f58
ngigijohn <joncarsel@gmail.com>
2022-07-06 17:38:54 +0300

    debug = false

--- slickskills/settings.py
+++ slickskills/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.2.9', '127.0.0.1', 'slick-skills.herokuapp.com']
 
